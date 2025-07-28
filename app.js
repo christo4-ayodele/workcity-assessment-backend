3a4b79690ecdbb7e69d38bd8fb07bb57ef3ffc5e
Jide <ayodelebabajidechris123@gmail.com>
2025-07-28 08:10:43 +0100

    Set up Express server and MongoDB connection with caching

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+//Middlewares
+
+//Enable Cross-Origin Resource Sharing (CORS)
+app.use(cors()); 
+
+//Automatically parse incoming JSON requests
+app.use(express.json()) 
+
+// ===== ROUTES =====
+// app.use('/api/auth', authRoutes);
+// app.use('/api/projects', projectRoutes);
+
+export default app;
